81479eea02c6bfe88ff7feeab326edda0e9c98d3
Arun Kumar Selvaraj <dev@ioak.org>
2019-07-19 11:51:25 +0530

    Updates

---
+++ precache-manifest.f8f6641674d9a0a985a96505caa380bf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "137f659d984b85a3f08ac8f73b1f57c2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7d16116dae33a2640972",
+    "url": "/static/css/main.37154f47.chunk.css"
+  },
+  {
+    "revision": "938e171763fef17fbb36",
+    "url": "/static/js/2.78bc9d8a.chunk.js"
+  },
+  {
+    "revision": "7d16116dae33a2640972",
+    "url": "/static/js/main.69bfed3b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "af4a349c4844aaf22912cfa4058a1cb5",
+    "url": "/static/media/placeholder-black2.af4a349c.svg"
+  },
+  {
+    "revision": "a3c6036a744827496af3e43cc55751f1",
+    "url": "/static/media/placeholder-white.a3c6036a.svg"
+  },
+  {
+    "revision": "ef310ed49d97bf9353069a47b9d5d3ce",
+    "url": "/static/media/placeholder-white2.ef310ed4.svg"
+  }
+]);
