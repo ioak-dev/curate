37ec27409792a440889f86c4f9ca765905df8da5
Arun Kumar Selvaraj <dev@ioak.org>
2019-07-27 20:00:00 +0530

    ATOM: Landing page UX design and content

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -7,45 +7,58 @@ export default class Home extends React.Component {
   render() {
     return (
       <>
-        <Navigation />
+        <Navigation transparent />
         <div className="home">
-          <div className="container">
-            {/* <div className="section one">
-              <h3>A bookmarking app like never before</h3>
-              <p>Take your link to the web wherever you go, with the most secure bookmark and note taking app</p>
-              <br />
-              <p><NavLink to="/login" className="navitem" activeClassName="active">Sign up</NavLink>&nbsp;for free</p>
-            </div>
-            <div className="section two">
-              <h3>Secure by design</h3>
-              <p>Designed with security as a primary factor from ground up</p>
-              <p>Your passwords never leaves your workstation</p>
-              <p>Our servers have zero knowledge about your password</p>
-            </div>
-            <div className="section three">
-              <h3>Premium features</h3>
-              <p>Get premium features free for lifetime. We standby free software for all</p>
-            </div>
-            <div className="section four">
-              <h3>Don't mix up personal and business</h3>
-              <p>With the help of workspaces, you can now work in isolated containers</p>
-            </div>
-            <div className="section five">
-              <h3>We love open source</h3>
-              <p>Our codebase is 100% available for the community to do peer review and audit for any security flaws</p>
-            </div>
-            <div className="section six">
-              <h3>Clean clutter free interface</h3>
-              <p>Modern interface with a clutter free design to keep you in focus on your activity</p>
-            </div>
-            <div className="section seven">
-              <h3>Tip of the iceberg</h3>
-              <p>We are evolving for the future. More features on the way. You can watch our roadmap here</p>
-            </div>
-            <div className="section eight">
-              <h3>Are you ready? Put your seat belts on</h3>
-              <p><NavLink to="/login" className="navitem" activeClassName="active">Sign up</NavLink>&nbsp;for free</p>
-            </div> */}
+          <div className="section parallax bg1" />
+          <div className="section static bg1 odd">
+            <h3>A bookmarking app like never before</h3>
+            <p>Take your link to the web wherever you go, with the most secure bookmark and note taking app</p>
+            <br />
+            <p><NavLink to="/login" className="navitem" activeClassName="active">Sign up</NavLink>&nbsp;for free</p>
+          </div>
+
+          <div className="section parallax bg bg2" />
+          <div className="section static bg2 even">
+            <h3>Secure by design</h3>
+            <p>Designed with security as a primary factor from ground up</p>
+            <p>Your passwords never leaves your workstation</p>
+            <p>Our servers have zero knowledge about your password</p>
+          </div>
+
+          <div className="section parallax bg3" />
+          <div className="section static bg3 odd">
+            <h3>Premium features</h3>
+            <p>Get premium features free for lifetime. We standby free software for all</p>
+          </div>
+
+          <div className="section parallax bg4" />
+          <div className="section static bg4 even">
+            <h3>Don't mix up personal and business</h3>
+            <p>With the help of workspaces, you can now work in isolated containers</p>
+          </div>
+
+          <div className="section parallax bg5" />
+          <div className="section static bg5 odd">
+            <h3>We love open source</h3>
+            <p>Our codebase is 100% available for the community to do peer review and audit for any security flaws</p>
+          </div>
+
+          <div className="section parallax bg6" />
+          <div className="section static bg6 even">
+            <h3>Clean clutter free interface</h3>
+            <p>Modern interface with a clutter free design to keep you in focus on your activity</p>
+          </div>
+
+          <div className="section parallax bg7" />
+          <div className="section static bg7 odd">
+            <h3>Tip of the iceberg</h3>
+            <p>We are evolving for the future. More features on the way. You can watch our roadmap here</p>
+          </div>
+
+          <div className="section parallax bg8" />
+          <div className="section static bg8 even">
+            <h3>Are you ready? Put your seat belts on</h3>
+            <p><NavLink to="/login" className="navitem" activeClassName="active">Sign up</NavLink>&nbsp;for free</p>
           </div>
         </div>
       </>
