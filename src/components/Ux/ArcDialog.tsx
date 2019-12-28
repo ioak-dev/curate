a589170b27e1703df68af3c77a221c1dbf935f09
Arun Kumar Selvaraj <dev@ioak.org>
2019-12-28 13:36:41 +0530

    Arc dialog footer layout refinements

--- src/components/Ux/ArcDialog.tsx
+++ src/components/Ux/ArcDialog.tsx
@@ -9,10 +9,19 @@ interface Props {
 }
 
 interface State {
-
+    views: any,
+    body?: any,
+    footer?: any
 }
 
 class ArcDialog extends Component<Props, State> {
+    constructor(props) {
+        super(props);
+        this.state = {
+            views: this.props.children
+        }
+    }
+
     componentWillReceiveProps(nextProps) {
         if (this.props.visible !== nextProps.visible) {
             if (nextProps.visible) {
@@ -27,10 +36,33 @@ class ArcDialog extends Component<Props, State> {
     componentDidMount(){
       document.addEventListener("keydown", this.escFunction, false);
     }
+
+    componentWillMount() {
+        this.initializeViews();
+    }
     
     componentWillUnmount(){
       document.removeEventListener("keydown", this.escFunction, false);
     }
+
+    initializeViews() {
+        let body: any[] = [];
+        let footer: any;
+        React.Children.toArray(this.state.views).forEach((node) => {
+            if (node.props.className === 'actions' || node.props.className === 'footer') {
+                footer = node;
+            } else {
+                body.push(node);
+            }
+        })
+        
+        this.setState({
+            body: body,
+            footer: footer
+        })
+
+    }
+
 
     escFunction = (event) => {
         if(event.keyCode === 27) {
@@ -47,10 +79,13 @@ class ArcDialog extends Component<Props, State> {
                 <div className={(this.props.visible ? "dialog show" : "dialog hide")}>
                     <div className={(this.props.visible ? "container": "container hidetext")}>
                         <div className="dialog-header" onClick={this.props.toggleVisibility}><i className="material-icons">close</i><div className="text-esc">esc</div></div>
-                        <div className="header-space"></div>
+                        {/* <div className="header-space"></div> */}
                         {/* {this.props.title && <div className="header">{this.props.title}<i className="material-icons" onClick={this.props.toggleVisibility}>close</i></div>} */}
                         <div className="dialog-body">
-                            {this.props.children}
+                            {this.state.body}
+                        </div>
+                        <div className="dialog-footer">
+                            {this.state.footer}
                         </div>
                     </div>
                 </div>
