f298136c06b64ce01f1cb0d77360c2dbd3f580cb
Arun Kumar Selvaraj <dev@ioak.org>
2020-02-04 15:30:54 +0530

    UX styles synchronize with ioak components

--- src/components/Ux/OakText.tsx
+++ src/components/Ux/OakText.tsx
@@ -13,16 +13,26 @@ interface Props {
     multiline?: boolean
 }
 function OakText(props: Props) {
+    function handleChange(event) {
+        event.preventDefault();
+        event.target.name = props.id;
+        event.target.value = event.target.textContent;
+        console.log(event.target);
+        props.handleChange(event);
+    }
     return (
         <div className="oak-text-field">
             <label>{props.label}</label>
             {!props.multiline && <input disabled={props.disabled} autoComplete="off"
                 className={(props.errorFields && props.errorFields[props.id] ? "error" : "") + (props.disabled ? " disabled" : "")}
                 type={props.type ? props.type : "text"} name={props.id} value={props.data[props.id]} onChange={props.handleChange}></input>}
-            
-            {props.multiline && <textarea disabled={props.disabled} rows={props.rows ? props.rows : 4}
+            {/* rows={props.rows ? props.rows : 4} */}
+            {props.multiline && <textarea disabled={props.disabled}
                 className={(props.errorFields && props.errorFields[props.id] ? "error" : "") + (props.disabled ? " disabled" : "")}
                 name={props.id} value={props.data[props.id]} onChange={props.handleChange}></textarea>}
+            {/* {props.multiline && <div contentEditable={props.disabled ? false : true} suppressContentEditableWarning={true}
+                className={"textarea " + (props.errorFields && props.errorFields[props.id] ? "error" : "") + (props.disabled ? " disabled" : "")}
+                onBlur={handleChange}>{props.data[props.id]}</div>} */}
         </div>
     )
 }
