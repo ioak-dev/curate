e35fca2c0f8e7b0c73ce98f091ba53680d89635d
Arun Kumar Selvaraj <dev@ioak.org>
2020-02-23 16:56:48 +0530

    precommit prettier config

--- src/components/Navigation/Desktop.tsx
+++ src/components/Navigation/Desktop.tsx
@@ -7,47 +7,86 @@ import Links from './Links';
 import { Authorization, Profile } from '../Types/GeneralTypes';
 import OakButton from '../../oakui/OakButton';
 
-interface Props {    
-    sendEvent: Function,
-    getAuth: Function,
-    addAuth: Function,
-    removeAuth: Function,
-    authorization: Authorization
-    getProfile: Function,
-    profile: Profile,
-    login: Function,
-    transparent: boolean,
-    logout: Function,
-    toggleSettings: any
+interface Props {
+  sendEvent: Function;
+  getAuth: Function;
+  addAuth: Function;
+  removeAuth: Function;
+  authorization: Authorization;
+  getProfile: Function;
+  profile: Profile;
+  login: Function;
+  transparent: boolean;
+  logout: Function;
+  toggleSettings: any;
 }
 
 const Desktop = (props: Props) => {
+  useEffect(() => {
+    props.getProfile();
+  }, []);
 
-    useEffect(() => {
-        props.getProfile();
-    }, []);
-
-    return (
-        <div className={(props.transparent ? "navbar desktop transparent" : "navbar desktop")}>
-            <div className="left">
-                {!props.transparent && props.profile.theme === 'theme_light' && <img className="logo" src={curate_black} alt="Curate logo" />}
-                {(props.transparent || props.profile.theme === 'theme_dark') && <img className="logo" src={curate_white} alt="Curate logo" />}
-                <Links authorization={props.authorization} profile={props.profile} />
-            </div>
-            <div className="right">
-                <div className="action">
-                    {props.authorization.isAuth && 
-                        <OakButton theme="default" variant="outline" small action={props.toggleSettings}><i className="material-icons">brush</i>Appearance</OakButton>}
-                    {props.authorization.isAuth && 
-                        <OakButton theme="default" variant="outline" small action={props.logout()}><i className="material-icons">power_settings_new</i>Logout</OakButton>}
-                    {!props.authorization.isAuth && 
-                        <OakButton theme="default" variant="outline" small action={() => props.login('signin')}><i className="material-icons">person</i>Login</OakButton>}
-                    {!props.authorization.isAuth && 
-                        <OakButton theme="default" variant="outline" small action={() => props.login('signup')}><i className="material-icons">person_add</i>Signup</OakButton>}
-                </div>
-            </div>
+  return (
+    <div
+      className={
+        props.transparent ? 'navbar desktop transparent' : 'navbar desktop'
+      }
+    >
+      <div className="left">
+        {!props.transparent && props.profile.theme === 'theme_light' && (
+          <img className="logo" src={curate_black} alt="Curate logo" />
+        )}
+        {(props.transparent || props.profile.theme === 'theme_dark') && (
+          <img className="logo" src={curate_white} alt="Curate logo" />
+        )}
+        <Links authorization={props.authorization} profile={props.profile} />
+      </div>
+      <div className="right">
+        <div className="action">
+          {props.authorization.isAuth && (
+            <OakButton
+              theme="default"
+              variant="outline"
+              small
+              action={props.toggleSettings}
+            >
+              <i className="material-icons">brush</i>Appearance
+            </OakButton>
+          )}
+          {props.authorization.isAuth && (
+            <OakButton
+              theme="default"
+              variant="outline"
+              small
+              action={props.logout()}
+            >
+              <i className="material-icons">power_settings_new</i>Logout
+            </OakButton>
+          )}
+          {!props.authorization.isAuth && (
+            <OakButton
+              theme="default"
+              variant="outline"
+              small
+              action={() => props.login('signin')}
+            >
+              <i className="material-icons">person</i>Login
+            </OakButton>
+          )}
+          {!props.authorization.isAuth && (
+            <OakButton
+              theme="default"
+              variant="outline"
+              small
+              action={() => props.login('signup')}
+            >
+              <i className="material-icons">person_add</i>Signup
+            </OakButton>
+          )}
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Desktop;
